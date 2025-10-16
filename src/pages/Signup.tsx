00181d37ce441c1593a8cd11db558e54c054ede7
Manuj Chadha <manujchadha7777@gmail.com>
2025-10-16 19:41:40 +0530

    ui fixes

--- src/pages/Signup.tsx
+++ src/pages/Signup.tsx
@@ -12,42 +12,42 @@ const navigate = useNavigate();
 
         <div className="text-left flex flex-col gap-[29px] mt-[31px]">
           <div className="flex flex-col relative">
-            <label className="absolute top-0 h-[21px] ml-[9px]   pl-[5px] text-[#6C25FF] text-[13px] bg-[#F7F8F9] w-auto -translate-y-1/2">
+            <label className="absolute top-0 h-[21px] ml-[9px]   pl-[5px] text-[#6C25FF] text-[12px] bg-[#F7F8F9] w-auto -translate-y-1/2">
               Full Name<span className="text-red-500 text-left">*</span>
             </label>
-            <input placeholder="Full name" type="text" value="Marry Doe" className="outline-1 rounded-md h-[40px] outline-[#CBCBCB] font-regular pl-[17px]" />
+            <input placeholder="Marry Doe" type="text" className="outline-1 rounded-md h-[40px] outline-[#CBCBCB] font-regular pl-[17px] placeholder:text-black placeholder:text-[12px]" />
           </div>
 
           <div className="flex flex-col relative">
-            <label className="absolute top-0 h-[21px] ml-[9px]   pl-[5px] text-[#6C25FF] text-[13px] bg-[#F7F8F9] w-auto -translate-y-1/2">
+            <label className="absolute top-0 h-[21px] ml-[9px]   pl-[5px] text-[#6C25FF] text-[12px] bg-[#F7F8F9] w-auto -translate-y-1/2">
               Phone number<span className="text-red-500 text-left">*</span>
             </label>
-            <input placeholder="Phone number" type="text" value="Marry Doe" className="outline-1 rounded-md h-[40px] outline-[#CBCBCB] font-regular pl-[17px]" />
+            <input placeholder="Marry Doe" type="text" className="outline-1 rounded-md h-[40px] outline-[#CBCBCB] font-regular pl-[17px] placeholder:text-black placeholder:text-[12px]" />
           </div>
 
           <div className="flex flex-col relative">
-            <label className="absolute top-0 h-[21px] ml-[9px]   pl-[5px] text-[#6C25FF] text-[13px] bg-[#F7F8F9] w-auto -translate-y-1/2">
+            <label className="absolute top-0 h-[21px] ml-[9px]   pl-[5px] text-[#6C25FF] text-[12px] bg-[#F7F8F9] w-auto -translate-y-1/2">
               Email address<span className="text-red-500 text-left">*</span>
             </label>
-            <input placeholder="Email address" type="email" value="Marry Doe" className="outline-1 rounded-md h-[40px] outline-[#CBCBCB] font-regular pl-[17px]" />
+            <input placeholder="Marry Doe" type="email" className="outline-1 rounded-md h-[40px] outline-[#CBCBCB] font-regular pl-[17px] placeholder:text-black placeholder:text-[12px]" />
           </div>
 
           <div className="flex flex-col relative">
-            <label className="absolute top-0 h-[21px] ml-[9px]   pl-[5px] text-[#6C25FF] text-[13px] bg-[#F7F8F9] w-auto -translate-y-1/2">
+            <label className="absolute top-0 h-[21px] ml-[9px]   pl-[5px] text-[#6C25FF] text-[12px] bg-[#F7F8F9] w-auto -translate-y-1/2">
               Password<span className="text-red-500 text-left">*</span>
             </label>
-            <input placeholder="Password" type="text" value="Marry Doe" className="outline-1 rounded-md h-[40px] outline-[#CBCBCB] font-regular pl-[17px]" />
+            <input placeholder="Marry Doe" type="text" className="outline-1 rounded-md h-[40px] outline-[#CBCBCB] font-regular pl-[17px] placeholder:text-black placeholder:text-[12px]" />
           </div>
 
           <div className="flex flex-col relative">
-            <label className="absolute top-0 h-[21px] ml-[9px]   pl-[5px] text-[#6C25FF] text-[13px] bg-[#F7F8F9] w-auto -translate-y-1/2">
+            <label className="absolute top-0 h-[21px] ml-[9px]   pl-[5px] text-[#6C25FF] text-[12px] bg-[#F7F8F9] w-auto -translate-y-1/2">
               Company name
             </label>
-            <input placeholder="Company name" type="text" value="Marry Doe" className="outline-1 rounded-md h-[40px] outline-[#CBCBCB] font-regular pl-[17px]" />
+            <input placeholder="Company name" type="text" className="outline-1 rounded-md h-[40px] outline-[#CBCBCB] font-regular pl-[17px] placeholder:text-black placeholder:text-[12px]" />
           </div>
 
           <div className="flex flex-col mt-[-9px]">
-            <p className="text-[13px]">Are you an Agency?<span className="text-red-500 text-left">*</span></p>
+            <p className="text-[12px]">Are you an Agency?<span className="text-red-500 text-left">*</span></p>
             <div className="flex gap-[23px] mt-[10px]">
               <label className="flex text-[14px] item-center"> 
                 <input type="radio" name="agency" className="mr-[12px] w-[22px] accent-[#6c25ff]" />
